6c958ab3d4761da9a1c760fa2baa02ea4fc657dd
Jiangen Jiao <joey.jiaojg@gmail.com>
2015-07-21 13:08:59 +0800

    server: An example django rest framework code
    Change-Id: I609e5fa616573b4a2f3385e765b3315b7bf34701

---
+++ server/apiServerForAngular/api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
--- server/apiServerForAngular/apiServerForAngular/urls.py
+++ server/apiServerForAngular/apiServerForAngular/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('api.urls')),
 )
